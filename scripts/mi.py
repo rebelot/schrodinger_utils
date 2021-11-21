7aadbd8f39483c9cc52bc3cc2e4aa5e9186a2564
Tommaso Laurenzi <tommaso.laurenzi@gmail.com>
2021-11-21 14:28:32 +0100

    update from xlence_scripts

--- scripts/mi.py
+++ scripts/mi.py
@@ -3,81 +3,142 @@
 import argparse
 import multiprocessing as mp
 import time
-from tqdm import tqdm
+from datetime import timedelta
+from functools import partial
 
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.feature_selection import mutual_info_regression
+from scipy.special import digamma
+from sklearn.neighbors import KDTree, NearestNeighbors, DistanceMetric
+from tqdm import tqdm
 
+# TODO: try using scipy.stats.boxcox to normalize MI
 
-def worker(x):  # -> (na * d)[:i]
-    return mutual_info_regression(
-        x, x[:, -1], discrete_features=False, n_neighbors=6, random_state=42
-    )
+DEFAULT_CA = "default_CA"
+SCHR_CA = "a.pt CA"
+MDA_CA = "name CA"
 
+def _worker(pair, k, metric):
+    return calculate_mi(*pair, k=k, metric=metric)
 
-def get_MI(x, njobs, dump=False, out="corr"):
+def get_MI(x, k=6, njobs=1, dump=False, metric='chebyshev'):
     nt, na, d = x.shape
-    x = x - x.mean(axis=0)  # center atoms on their mean position
+    x = x - x.mean(axis=0)  # get atom fluctuations oround their mean position
     # normalize fluctuation interval between 0, 1 WARNING: is this required?
     x = (x - x.min(axis=0)) / np.ptp(x, axis=0)
-    x = x.reshape(nt, na * d)
-    MI = np.zeros((na * d, na * d))
+    a = x.transpose((1, 0, -1))  # -> (na, nt, d)
+
+    pairs = ((a[i], a[j]) for i in range(na) for j in range(i))
+
+    MI = np.zeros((na, na))
 
     start_time = time.time()
     with mp.Pool(processes=njobs) as pool:
-        res = pool.imap(worker, (x[:, : i + 1] for i in range(na * d)))
-        for i, r in tqdm(enumerate(res), total=na * d):
-            MI[i, : i + 1] = r
-            MI[: i + 1, i] = r
+        iterations = int(na * (na - 1) / 2)
+        res = pool.imap(partial(_worker, k=k, metric=metric), pairs, chunksize=iterations // njobs)
+        MI[np.tril_indices_from(MI, -1)] = np.fromiter(
+            tqdm(res, total=iterations),
+            dtype=np.float64
+        )
+    print()
     print(
-        f"finished {na} atoms for {nt} frames in", time.time() - start_time, "seconds"
+        f"finished {na} atoms for {nt} frames in {timedelta(seconds=time.time() - start_time)}"
     )
+
+    MI += MI.T
 
     if dump:
         import pickle
 
-        with open(out + ".pickle", "wb") as f:
+        with open(dump + ".pickle", "wb") as f:
             pickle.dump(MI, f)
 
     # postprocess
-    # (ai, xi, aj, xj)
-    MI = MI.reshape(na, d, na, d).mean(axis=(1, 3))
-    MI = np.sqrt(1 - np.exp(-2 * MI))  # eq. 9 from https://www.mpibpc.mpg.de/276284/paper_generalized_corr_lange.pdf
+    # eq. 9 from https://www.mpibpc.mpg.de/276284/paper_generalized_corr_lange.pdf
+    # MI = np.sqrt(1 - np.exp(-2 * MI))
+    MI /= MI.max()
+    MI[np.diag_indices_from(MI)] = 1
     return MI
+
+
+def calculate_mi(x, y, k=6, metric='chebyshev'):
+    """
+    Calculate Mutual Information between two continuous multidimensional random variables
+    using Nearest Neighbour approximation for entropy as described in [1].
+
+    :param x, y: np.ndarray of shape (n_samples, n_dim)
+    :kwarg    k: number of neighbors to use to estimate entropy
+    :kwarg metric: metric to calculate distances between points, see sklearn KDTree metrics
+    :return: scalar MI score I(x;y)
+
+    References
+    ----------
+    [1] A. Kraskov, H. Stogbauer and P. Grassberger, "Estimating mutual
+        information". Phys. Rev. E 69, 2004.
+    """
+    # x, y = (n, d)
+    if metric == 'chebyshev':
+        xy = np.hstack((x, y))  # (n, 2*d)
+        nn = NearestNeighbors(metric="chebyshev", n_neighbors=k)
+        nn.fit(xy)
+        radi = nn.kneighbors()[0]
+        radi = np.nextafter(radi[:, -1], 0)
+    else:
+        metric = DistanceMetric.get_metric(metric)
+        xx = metric.pairwise(x)
+        yy = metric.pairwise(y)
+        zz = np.maximum(xx, yy)
+        zz.sort(axis=1)
+        radi = np.nextafter(zz[:, k], 0)
+
+    kd = KDTree(x, metric=metric)
+    nx = kd.query_radius(x, radi, count_only=True, return_distance=False)
+    nx = np.array(nx) - 1.0
+
+    kd = KDTree(y, metric=metric)
+    ny = kd.query_radius(y, radi, count_only=True, return_distance=False)
+    ny = np.array(ny) - 1.0
+
+    n_samples = len(x)
+
+    mi = (
+        digamma(n_samples)
+        + digamma(k)
+        - np.mean(digamma(nx + 1))
+        - np.mean(digamma(ny + 1))
+    )
+
+    return max(0, mi)
 
 
 def get_cormat(x):
     """
     Get covariance matrix of atom fluctuations
-    :param x: atom positions over time
-    :type  x: np.array of shape (time, atoms, dims)
-    :return : the normalized covariance matrix
-    :return type: np.array (atoms, atoms)
+
+    :param x: atom positions over time, np.ndarray of shape (n_time, n_atoms, n_dims)
+    :return: the normalized covariance matrix, np.ndarray of shape (n_atoms, n_atoms)
     """
-    # dots = np.zeros((na, na))
-    # for i in range(nt):
-    #     dot = np.dot(fluct[i], fluct[i].T)
-    #     dots = dots + dot
+
     fluct = x - np.mean(x, axis=0)
     dots = 1 / x.shape[0] * np.einsum("ijk,ilk->jl", fluct, fluct)
+    
+    std = np.sqrt(np.diagonal(dots))
+    std_matrix = np.outer(std, std)
+    corr_matrix = dots / std_matrix
+    # corr_matrix = np.abs(corr_matrix)
 
-    diagonal = np.diag(dots)
-    norm_matrix = np.outer(diagonal, diagonal)
-    norm_matrix = np.sqrt(np.absolute(norm_matrix))
-    corr_matrix = np.divide(dots, norm_matrix)
-    return corr_matrix
+    return np.clip(corr_matrix, -1, 1)
 
 
 def preproc_schrodinger(args):
     from schrodinger.application.desmond.packages import topo, traj, traj_util
 
-    align_sel = "a.pt CA" if args.align == "CA" else args.align
-    corr_sel = "a.pt CA" if args.asl == "CA" else args.asl
+    align_sel = SCHR_CA if args.align == DEFAULT_CA else args.align
+    corr_sel = [SCHR_CA] if args.asl == DEFAULT_CA else args.asl
 
     if args.t:
         msys, cms = topo.read_cms(args.cms)
-        trj = traj.read_traj(args.t)
+        trj = traj.read_traj(args.t[0])
     else:
         msys, cms, trj = traj_util.read_cms_and_traj(args.cms)
 
@@ -95,8 +156,11 @@ def preproc_schrodinger(args):
         ref = cms.extract(cms.select_atom(align_sel)).getXYZ()
         trj = topo.superimpose(msys, fit_gids, trj, ref)
 
-    gids = topo.asl2gids(cms, corr_sel)
-    x = np.array([fr.pos(gids) for fr in trj])  # type: ignore
+    x = []
+    for sel in corr_sel:
+        gids = topo.asl2gids(cms, sel)
+        x.append(np.array([fr.pos(gids) for fr in trj]))  # type: ignore
+    x = np.concatenate(x, axis=1)
     return x
 
 
@@ -104,20 +168,26 @@ def preproc_mda(args):
     import MDAnalysis as mda
     from MDAnalysis.analysis import align
 
-    align_sel = "name CA" if args.align == "CA" else args.align
-    corr_sel = "name CA" if args.asl == "CA" else args.asl
+    align_sel = MDA_CA if args.align == DEFAULT_CA else args.align
+    corr_sel = [MDA_CA] if args.asl == DEFAULT_CA else args.asl
+
 
     top = args.cms
-    traj = (
-        args.t if isinstance(args.t, list) else [args.t]
-    )  # better way to handle this using argparse action='append'
     slicer = (
         slice(*[int(i) if i else None for i in args.s.split(":")])
         if args.s
         else slice(None, None)
     )
 
-    U = mda.Universe(top, *traj)
+    U = mda.Universe(top, *args.t)
+
+    # validate selection
+    try:
+        U.select_atoms(align_sel)
+        for sel in corr_sel:
+            U.select_atoms(sel)
+    except mda.SelectionError as exc:
+        raise exc
 
     if args.align:
         print(f'Aligning trajectory to "{align_sel}"')
@@ -126,21 +196,34 @@ def preproc_mda(args):
         align.AlignTraj(mobile, U, select=align_sel, in_memory=True).run()
         U = mobile
 
-    U.trajectory[0]
-    atoms = U.select_atoms(corr_sel)
-    x = np.array([atoms.positions for _ in U.trajectory[slicer]])
+    if args.com_by_res:
+        posgetter = lambda atoms: atoms.center_of_mass(compound='residues')
+    else:
+        posgetter = lambda atoms: atoms.positions
+
+    x = []
+    for sel in corr_sel:
+        U.trajectory[0]
+        atoms = U.select_atoms(sel)
+        x.append(np.array([posgetter(atoms) for _ in U.trajectory[slicer]]))
+    x = np.concatenate(x, axis=1)
     return x
 
 
 def main():
     parser = argparse.ArgumentParser(
         description="""
-            Calculate generalized correlations based on mutual information of selected atoms fluctuations
-            example:
-            mi.py my_md.pdb corr_out -t my_md.xtc -backend mda -j 16 -s ::10 -align "name CA and segid A" -asl "name CA"
-            """
+    Calculate generalized correlations based on mutual information of selected atoms fluctuations.
+    example:
+
+    mi.py my_md.pdb corr_out -t my_md.xtc -backend mda -j 16 -s ::10 -align "name CA and segid A" -asl "name CA and segid A" "name CB and segid B" -corr
+    """,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
     )
-    parser.add_argument("cms", help="input topology file")
+    parser.add_argument(
+        "cms",
+        help="input topology file: cms for schrodinger backend, or any format supported by MDAnalysis",
+    )
     parser.add_argument("out", help="base name for the output")
     parser.add_argument(
         "-backend",
@@ -150,8 +233,9 @@ def main():
     )
     parser.add_argument(
         "-t",
-        help="trj dir, this is mandatory if backend is MDAnalysis",
+        help="trj dir. If backend is MDAnalysis, multiple trajectories to be concatenated can be supplied",
         metavar="trajectory",
+        nargs="+",
     )
     parser.add_argument(
         "-j",
@@ -160,7 +244,12 @@ def main():
         metavar="N cores",
         default=1,
     )
-    parser.add_argument("-asl", help="atom selection, default CA", default="CA")
+    parser.add_argument(
+        "-asl",
+        help="atom selection. You can specify multiple selections that will be concatenated sequentially and will appear as contiguous blocks in the results. Default CA",
+        nargs="+",
+        default=DEFAULT_CA,
+    )
     parser.add_argument("-s", help="slicer", metavar="START:END:STEP")
     parser.add_argument(
         "-pickle", help="dump unmodified MI matrix to pickle file", action="store_true"
@@ -171,9 +260,22 @@ def main():
     parser.add_argument(
         "-align",
         help="asl to align trajectory, default CA",
-        const="CA",
+        const=DEFAULT_CA,
         nargs="?",
         metavar="ASL",
+    )
+    parser.add_argument(
+        "-k", help="number of neighbors to use to estimane entropy, default 6", type=int, default=6, metavar='N'
+    )
+    parser.add_argument(
+        "-metric",
+        help="scikit-learn distance metric to estimate distances between fluctuations. Default chebychev",
+        default='chebyshev'
+    )
+    parser.add_argument(
+        "-com_by_res",
+        action='store_true',
+        help='MI will be calculated on the centers of mass of residues specified by -asl. (MDA Only)'
     )
     args = parser.parse_args()
 
@@ -185,8 +287,8 @@ def main():
     x = preproc(args)
     nt, na, d = x.shape
 
-    print('Calculating Generalized Correlations ...')
-    MI = get_MI(x, args.j, dump=args.pickle, out=args.out)
+    print("Calculating Generalized Correlations ...")
+    MI = get_MI(x, k=args.k, njobs=args.j, dump=args.pickle and args.out, metric=args.metric)
     print("Done.")
 
     with open(args.out + "_MI.dat", "w") as f:
@@ -195,11 +297,11 @@ def main():
             f.write(" ".join(str(i) for i in row))
             f.write("\n")
 
-    fig = plt.figure(figsize=(15, 15))
+    fig = plt.figure(dpi=800)
     fig.suptitle("Mutual Information")
-    plt.imshow(MI, origin="lower", cmap='inferno')
+    plt.imshow(MI, origin="lower", cmap="inferno")
     plt.colorbar()
-    fig.tight_layout()
+    # fig.tight_layout()
     plt.savefig(args.out + "_MI.png")
 
     if args.corr:
@@ -211,35 +313,30 @@ def main():
             for row in C:
                 f.write(" ".join(str(i) for i in row))
                 f.write("\n")
-        fig = plt.figure(figsize=(15, 15))
+        fig = plt.figure(dpi=800)
         fig.suptitle("Correlation Matrix")
-        plt.imshow(C, origin="lower", cmap='inferno')
+        plt.imshow(C, origin="lower", cmap="bwr")
         plt.colorbar()
-        fig.tight_layout()
+        # fig.tight_layout()
         plt.savefig(args.out + "_Cor.png")
 
-        fig = plt.figure(figsize=(15, 15))
-        fig.suptitle("MI/Cor Matrix")
-        plt.imshow(np.tril(MI) + np.triu(C, k=1), origin="lower", cmap='inferno')
+        fig = plt.figure(dpi=800)
+        fig.suptitle("MI $vs$ abs(Cor) Matrix")
+        plt.imshow(np.tril(MI) + np.triu(np.abs(C), k=1), origin="lower", cmap="inferno")
         plt.colorbar()
-        fig.tight_layout()
+        # fig.tight_layout()
         plt.savefig(args.out + "_MICor.png")
 
     print("All done.")
-    print(r"""
-            ____
-            /____ `\
-           ||_  _`\ \
-     .-.   `|O, O  ||
-     | |    (/    -)\
-     | |    |`-'` |\`
-  __/  |    | _/  |
- (___) \.  _.\__. `\___
- (___)  )\/  \    _/  ~\.
- (___) . \   `--  _   `\
-  (__)-    ,/        (   |
-       `--~|         |   |
-           |         |   | ")
-    """)
+    print( r"""
+|
+|  .-.-.   .-.-.   .-.-.   .-.-.   .-.-.   .-.-.   .-.-.  
+| / / \ \ / / \ S T A Y  I N F O R M E D\ / / \ \ / / \ \ 
+|`-'   `-`-'   `-`-'   `-`-'   `-`-'   `-`-'   `-`-'   `-`
++---------------------------------------------------------->
+    """
+    )
+
+
 if __name__ == "__main__":
     main()
