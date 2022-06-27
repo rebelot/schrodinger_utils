a6ca029e28ce9b50a090648be5f42c772e31a294
Tommaso Laurenzi <tommaso.laurenzi@gmail.com>
2022-06-27 09:36:12 +0200

    feat(trj_interactions): add cumulative results

--- scripts/trj_interactions.py
+++ scripts/trj_interactions.py
@@ -1,22 +1,35 @@
-from schrodinger.application.desmond.packages import topo, traj_util, traj, analysis
+import argparse
+import sys
+from itertools import chain
+
+import matplotlib.pyplot as plt
+import numpy as np
+from schrodinger.application.desmond.packages import analysis, topo, traj, traj_util
 from schrodinger.structutils import analyze
 from tabulate import tabulate
-import numpy as np
-import matplotlib.pyplot as plt
-import sys
-import argparse
+
+TITLE = {
+    "HB": "H-Bonds",
+    "SB": "Salt Bridges",
+    "PiPi": "Pi-Pi interactions",
+    "CatPi": "Pi-Cation interactions",
+    "HPho": "Hydrophobic interactions",
+    "all": "All interactions",
+}
 
 
-class InteractionOutput():
+class InteractionOutput:
     """
     IO = InteractionOutput(data, cms)
     bonddict = IO.bonddict(aids1)
     em, keys = IO.existence_map(bonddict)
     nbonds = IO.count_bonds()
     """
-    def __init__(self, data, cms):
+
+    def __init__(self, data, btype, cms):
         self.data = self._preproc(data)
         self.cms = cms
+        self.btype = btype
         self.atoms = list(cms.atom)
 
     def _preproc(self, data):
@@ -61,6 +74,8 @@ class InteractionOutput():
                     key = (key1, key2)
                     bonddict.setdefault(key, [])
                     bonddict[key].append(f)
+        if self.btype == "all":
+           return { k: sorted(list(set(v))) for k, v in bonddict } 
         return bonddict
 
     def existence_map(self, bonddict):
@@ -78,81 +93,61 @@ class InteractionOutput():
         return [len(b) for b in self.data]
 
 def plot_em(ax, em, keys, btype, times):
-    ax.imshow(em, aspect='auto')
+    ax.imshow(em, aspect="auto")
     ax.set_yticks(range(len(keys)))
-    ax.set_yticklabels([bkey2str(key) for key in keys], fontdict={'fontsize': 5})
+    ax.set_yticklabels([bkey2str(key) for key in keys], fontdict={"fontsize": 5})
     nlab = 5
     step = int(len(times) / (nlab - 1))
     pos = np.arange(0, len(times), step)
     ax.set_xticks(pos)
     ax.set_xticklabels(np.round(times[::step], decimals=2))
-    ax.set_xlabel('time (ns)')
-    if btype == 'HB':
-        ax.set_title('H-Bonds')
-    elif btype == 'SB':
-        ax.set_title('Salt Bridges')
-    elif btype == 'PiPi':
-        ax.set_title('Pi-Pi Interactions')
-    elif btype == 'CatPi':
-        ax.set_title('Pi-Cation Interactions')
-    elif btype == 'HPho':
-        ax.set_title('Hydrophobic Interactions')
+    ax.set_xlabel("time (ns)")
+    ax.set_title(TITLE[btype])
     # ax.set_ylabel('bond')
 
 
 def plot_allbonds(ax, allbonds, btype, times):
-    if btype == 'HB':
-        ax.set_title('H-Bonds')
-    elif btype == 'SB':
-        ax.set_title('Salt Bridges')
-    elif btype == 'PiPi':
-        ax.set_title('Pi-Pi Interactions')
-    elif btype == 'CatPi':
-        ax.set_title('Pi-Cation Interactions')
-    elif btype == 'HPho':
-        ax.set_title('Hydrophobic Interactions')
+    ax.set_title(TITLE[btype])
     ax.plot(times, allbonds)
-    ax.set_xlabel('time (ns)')
-    ax.set_ylabel('# bonds')
+    ax.set_xlabel("time (ns)")
+    ax.set_ylabel("# bonds")
 
 
-def write_em(fh, em, keys, times):
-    tstr = ','.join(str(t) for t in times)
-    fh.write('bond,'+tstr+'\n')
-    for bond, key in zip(em, keys):
-        frames = ','.join(str(v) for v in bond)
-        fh.write(f'{bkey2str(key)}, {frames}\n')
+def write_em(fname: str, em, keys, times):
+    with open(fname, "w") as fh:
+        tstr = ",".join(str(t) for t in times)
+        fh.write("bond," + tstr + "\n")
+        for bond, key in zip(em, keys):
+            frames = ",".join(str(v) for v in bond)
+            fh.write(f"{bkey2str(key)}, {frames}\n")
 
 
-def write_allbonds(fh, all_bonds, times):
-    for t, bond_num in zip(times, all_bonds):
-        fh.write(f'{t} {bond_num}\n')
+def write_allbonds(fname, all_bonds, times):
+    with open(fname, "w") as fh:
+        for t, bond_num in zip(times, all_bonds):
+            fh.write(f"{t} {bond_num}\n")
 
 
-def write_summary(fh, em, keys, btype, args):
-    tdata = [[" ".join([str(i) for i in key[0]]),
-              " ".join([str(i) for i in key[1]]),
-              f'{row.mean():.2%}'] for key, row in zip(keys, em)]
-    table = tabulate(reversed(tdata), headers=(args.g1, args.g2, '%'))
+def write_summary(fname, em, keys, btype, args):
+    tdata = [
+        [
+            " ".join([str(i) for i in key[0]]),
+            " ".join([str(i) for i in key[1]]),
+            f"{row.mean():.2%}",
+        ]
+        for key, row in zip(keys, em)
+    ]
+    table = tabulate(reversed(tdata), headers=(args.g1, args.g2, "%"))
+    with open(fname, "w") as fh:
+        fh.write(f"{args.out} {TITLE[btype]} summary.\n")
+        fh.write(table)
+        fh.write("\n\n")
 
 
-    if btype == 'HB':
-        string = 'H-Bonds'
-    elif btype == 'SB':
-        string = "Salt Bridges"
-    elif btype == 'PiPi':
-        string = 'Pi-Pi interactions'
-    elif btype == 'CatPi':  # CatPi
-        string = 'Pi-Cation interactions'
-    elif btype == 'HPho':
-        string = 'Hydrophobic'
-    fh.write(f'{args.out} {string} summary.\n')
-    fh.write(table)
-    fh.write('\n\n')
-
-    
 def bkey2str(key):
-    return f'{" ".join([str(i) for i in key[0]])} - {" ".join([str(i) for i in key[1]])}'
+    return (
+        f'{" ".join([str(i) for i in key[0]])} - {" ".join([str(i) for i in key[1]])}'
+    )
 
 
 def main():
@@ -161,23 +156,48 @@ def main():
                 between the specified atoms.  If group2 is given, then this
                 script will return interacions between the two groups of
                 atoms.  Else, nonbonded interactions within a single group of
-                atoms will be returned.""")
+                atoms will be returned."""
+    )
     parser.add_argument("cms", help="Input cms file")
-    parser.add_argument("out", help="output base filename (descriptive extensions are added automatically)")
-    parser.add_argument('-t', help="Trajectory dir")
-    parser.add_argument("-g1", help="define group 1", metavar="ASL", default='all')
+    parser.add_argument(
+        "out",
+        help="output base filename (descriptive extensions are added automatically)",
+    )
+    parser.add_argument("-t", help="Trajectory dir")
+    parser.add_argument("-g1", help="define group 1", metavar="ASL", default="all")
     parser.add_argument("-g2", help="define group 2", metavar="ASL", default=None)
-    parser.add_argument("-p", "--plot", help="plot bond nr over time", action="store_true")
-    parser.add_argument("-rkey", help="comma-separated indexes to slice the key that is used to identify unique reisudes:\
+    parser.add_argument(
+        "-p", "--plot", help="plot bond nr over time", action="store_true"
+    )
+    parser.add_argument(
+        "-rkey",
+        help="comma-separated indexes to slice the key that is used to identify unique reisudes:\
         key = [atom.chain, atom.pdbres, atom.resnum, atom.element, atom.index] \
         they may be specified indepentently for both groups in the order in which they appear:\
-        e.g.: -rkey 0,3 -rkey 0,0 (0 means all)", action="append")
-    parser.add_argument('-r', help="Filter results at residue level (alias for '-rkey 0,3[,0,3])", action="store_true")
-    parser.add_argument("-b", help="comma-separated list of interaction types to compute:\
-            default is hb,sb,pipi,catpi [optional: hpho]", default='hb,sb,pipi,catpi')
-    parser.add_argument("-th", help="only report the existence of bonds with an existence % above N", metavar='N', type=float)
+        e.g.: -rkey 0,3 -rkey 0,0 (0 means all)",
+        action="append",
+    )
+    parser.add_argument(
+        "-r",
+        help="Filter results at residue level (alias for '-rkey 0,3[,0,3])",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-b",
+        help="comma-separated list of interaction types to compute:\
+            default is hb,sb,pipi,catpi [optional: hpho]",
+        default="hb,sb,pipi,catpi",
+    )
+    parser.add_argument(
+        "-th",
+        help="only report the existence of bonds with an existence % above N",
+        metavar="N",
+        type=float,
+    )
     # parser.add_argument("-em", help="output indexed existence map", action="store_true")
-    parser.add_argument('-s', help='slice trajectory', metavar='START:END:STEP', default='::')
+    parser.add_argument(
+        "-s", help="slice trajectory", metavar="START:END:STEP", default="::"
+    )
 
     args = parser.parse_args()
 
@@ -187,7 +207,7 @@ def main():
     else:
         msys, cms, trj = traj_util.read_cms_and_traj(args.cms)
 
-    slicer = slice(*[int(v) if v else None for v in args.s.split(':')])
+    slicer = slice(*[int(v) if v else None for v in args.s.split(":")])
     trj = trj[slicer]
     times = np.array(list(fr.time / 1000.0 for fr in trj))
 
@@ -198,37 +218,50 @@ def main():
 
     analyzers = []
     btype = []
-    for a in args.b.split(','):
-        if a == 'hb':
-            analyzers.append(analysis.HydrogenBondFinder(msys, cms, aids1=aids1, aids2=aids2))
-            btype.append('HB')
-        elif a == 'sb':
-            analyzers.append(analysis.SaltBridgeFinder(msys, cms, aids1=aids1, aids2=aids2))
-            btype.append('SB')
-        elif a == 'pipi':
+    for a in args.b.split(","):
+        if a == "hb":
+            analyzers.append(
+                analysis.HydrogenBondFinder(msys, cms, aids1=aids1, aids2=aids2)
+            )
+            btype.append("HB")
+        elif a == "sb":
+            analyzers.append(
+                analysis.SaltBridgeFinder(msys, cms, aids1=aids1, aids2=aids2)
+            )
+            btype.append("SB")
+        elif a == "pipi":
             analyzers.append(analysis.PiPiFinder(msys, cms, aids1=aids1, aids2=aids2))
-            btype.append('PiPi')
-        elif a == 'catpi':
+            btype.append("PiPi")
+        elif a == "catpi":
             analyzers.append(analysis.CatPiFinder(msys, cms, aids1=aids1, aids2=aids2))
-            btype.append('CatPi')
-        elif a == 'hpho':
-            analyzers.append(analysis.HydrophobicInter(msys, cms, prot_asl=asl1, lig_asl=asl2))
-            btype.append('HPho')
+            btype.append("CatPi")
+        elif a == "hpho":
+            analyzers.append(
+                analysis.HydrophobicInter(msys, cms, prot_asl=asl1, lig_asl=asl2)
+            )
+            btype.append("HPho")
 
     out = analysis.analyze(trj, *analyzers)
 
+    out.append(chain(*out))
+    btype.append("all")
+
     if args.rkey:
-        rkeys = [int(j) if int(j) != 0 else None for i in args.rkey for j in i.split(',')]
+        rkeys = [
+            int(key) if int(key) != 0 else None
+            for keys in args.rkey
+            for key in keys.split(",")
+        ]
         assert len(rkeys) == 2 or len(rkeys) == 4
         rkeys[2:4] = rkeys[:3] if (len(rkeys) == 2 and not args.g2) else rkeys[2:4]
     else:
-        rkeys = [0,None,0,None]
+        rkeys = [0, None, 0, None]
 
     if args.r:
-        rkeys = [0,3,0,3]
+        rkeys = [0, 3, 0, 3]
 
     for data, btype in zip(out, btype):
-        InterOut = InteractionOutput(data, cms)
+        InterOut = InteractionOutput(data, btype, cms)
         bonddict = InterOut.bonddict(aids1, *rkeys)
         em, keys = InterOut.existence_map(bonddict)
         allbonds = InterOut.count_bonds()
@@ -238,29 +271,26 @@ def main():
             indexes = np.nonzero(existence * 100 >= args.th)[0]
             em, keys = em[indexes, :], [keys[i] for i in indexes]
 
+        f_allbonds: str = args.out + f"_{btype}-ab.csv"
+        f_em: str = args.out + f"_{btype}-em.csv"
+        f_summary: str = args.out + f"_{btype}.txt"
 
-        fh_allbonds = open(args.out + f'_{btype}-ab.csv', 'w')
-        fh_em = open(args.out + f'_{btype}-em.csv', 'w')
-        fh_summary = open(args.out + f'_{btype}.txt', 'w')
-
-        write_summary(fh_summary, em, keys, btype, args)
-        fh_summary.close()
-
-        write_em(fh_em, em, keys, times)
-        fh_em.close()
-
-        write_allbonds(fh_allbonds, allbonds, times)
-        fh_allbonds.close()
+        write_summary(f_summary, em, keys, btype, args)
+        write_em(f_em, em, keys, times)
+        write_allbonds(f_allbonds, allbonds, times)
 
         if keys and args.plot:
 
             fig, ax = plt.subplots(1, figsize=(10, 0.1 * len(keys) + 1))
             plot_em(ax, em, keys, btype, times)
             plt.tight_layout()
-            fig.savefig(args.out + f'_{btype}-em.png')
+            fig.savefig(args.out + f"_{btype}-em.png")
             fig, ax = plt.subplots(1)
             plot_allbonds(ax, allbonds, btype, times)
-            fig.savefig(args.out + f'_{btype}-ab.png')
+            fig.savefig(args.out + f"_{btype}-ab.png")
+
+
+
 
 if __name__ == "__main__":
     main()
